9e3e6115b5e3a14cf7c6ac1428353a653d2f4744
ManuelGraziosi <manuel.graziosi@gmail.com>
2025-09-16 18:37:47 +0200

    feat: ese5 completed

--- bonus/ese5/js/script.js
+++ bonus/ese5/js/script.js
@@ -49,39 +49,46 @@ const checkFidelityCard = prompt("Hai la carta fedeltà? (si/no)");
 let productPrice = 0;
 let discountPercentage = 0;
 
-// Definizione del prezzo del prodotto scelto
-if (productStr === "pane") {
-    productPrice = 1.50;
-} else if (productStr === "latte") {
-    productPrice = 1.20;
-} else if (productStr === "uova") {
-    productPrice = 2.60;
+if (!productStr || !quantityStr || !checkFidelityCard) {
+    console.log("ERRORE: dati inseriti non validi");
 } else {
-    productPrice = null;
-}
 
-// Calcolo del totale prima dello sconto
-const subTotal = productPrice * quantityStr;
-
-// Calcolo della percentuale di sconto
-if (checkFidelityCard === "si") {
-    if (subTotal < 20) {
-        discountPercentage = 5;
+    // Definizione del prezzo del prodotto scelto
+    if (productStr === "pane") {
+        productPrice = 1.50;
+    } else if (productStr === "latte") {
+        productPrice = 1.20;
+    } else if (productStr === "uova") {
+        productPrice = 2.60;
     } else {
-        discountPercentage = 10;
+        throw new Error("ERR: scleta non valida. Ricarica la pagina");
     }
-} else if (checkFidelityCard === "no") {
-    discountPercentage = 0
+
+    // Calcolo del totale prima dello sconto
+    const subTotal = productPrice * quantityStr;
+
+    // Calcolo della percentuale di sconto
+    if (checkFidelityCard === "si") {
+        if (subTotal < 20) {
+            discountPercentage = 5;
+        } else {
+            discountPercentage = 10;
+        }
+    } else if (checkFidelityCard === "no") {
+        discountPercentage = 0
+    } else {
+        throw new Error("ERR2: scelta non valida. Ricarica la pagina");
+    }
+
+    // calcolo del totale in funzione dello sconto
+    const discount = subTotal * discountPercentage / 100;
+    const total = subTotal - discount;
+
+    // Stampa dello scontrino
+    console.log(`SCONTRINO
+        ${quantityStr} * ${productPrice}€ -> ${subTotal}€
+        ${productStr}
+        SCONTO: ${discountPercentage}% -> ${discount}€
+        TOTALE: ${total}€
+        `)
 }
-
-// calcolo del totale in funzione dello sconto
-const discount = subTotal * discountPercentage / 100;
-const total = subTotal - discount;
-
-// Stampa dello scontrino
-console.log(`SCONTRINO
-    ${quantityStr} * ${productPrice}€ -> ${subTotal}
-    ${productStr}
-    SCONTO: ${discountPercentage}% -> ${discount}€
-    TOTALE: ${total}€
-    `)
